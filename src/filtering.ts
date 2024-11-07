ffd2e0f0eaf4147c9e57dcbb7bc586e4759d59da
ashiqee <ashiq.buet73@gmail.com>
2024-11-07 06:41:03 +0600

    filtering query

--- src/filtering.ts
+++ src/filtering.ts
@@ -8,10 +8,29 @@ const filtering =async()=>{
     
 
     const result=await prisma.post.findMany({
+
+        // AND Filtering 
+
+        // where:{
+        //     AND:[
+        //         {
+        //             title:{contains:"title"},
+        //             
+        //         }
+        // {
+        //     published:true
+        // }
+        
+        //     ]
+        // }
+
+        // OR Filtring 
         where:{
-            AND:[
+            OR:[
                 {
                     title:{contains:"title"},
+                
+                },{
                     published:true
                 }
             ]
@@ -19,8 +38,62 @@ const filtering =async()=>{
 
     })
 
-    console.log(result);
+    // console.log(result);
+
+    const notFiltering= await prisma.post.findMany({
+        where:{
+            NOT:[
+                {
+                    title:{
+                        contains:"title"
+                    }
+                }
+            ]
+        }
+    })
+
+    // startsWith, endswith, equals
+
+    const startsWith = await prisma.user.findMany({
+        where:{
+            email:{
+                // startsWith:"u",
+                // endsWith:"ph.com"
+                equals:"user2@ph.com"
+            }
+        }
+    })
     
+
+    const userNameArray = ['user1','user2','user5'];
+
+    const userNamesByArray = await prisma.user.findMany({
+        where:{
+            username:{
+                in:userNameArray
+            }
+        }
+    })
+
+    const inDepthData = await prisma.user.findMany({
+        where:{
+            id:1
+        },
+        include:{
+            post:{
+                include:{
+                    postCategory:{
+                        include:{
+                            category:true
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+
+console.dir(inDepthData,{depth:Infinity});
 
 
 }
